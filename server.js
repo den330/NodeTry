61b6cabf390883b967ba675d4e7762b5fb7a3865
den330 <wo131485508@qq.com>
2023-01-31 20:26:33 -0500

    route

--- server.js
+++ server.js
@@ -2,34 +2,42 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
+const cors = require('cors');
 
 app.use(logger);
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500', 'https://www.google.ca'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if(req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if(req.accepts('json')) {
+        res.json({ error: "404 Not Found"});
+    } else {
+        res.type('txt').send('404 not found');
+    }
 });
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301,'/new-page.html');
-});
-
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello world!');
-});
-
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`listen on ${PORT}`));
