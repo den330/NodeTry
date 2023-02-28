da09f56150a16f702b027aa335476068c422d3bf
den330 <wo131485508@qq.com>
2023-02-27 22:33:33 -0500

    add files

---
+++ controllers/logoutController.js
@@ -0,0 +1,32 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data } 
+}
+
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogout = async (req, res) => {
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(204);
+    console.log(`cookie jwt is ${cookies.jwt}`);
+    const refreshToken = cookies.jwt;
+
+    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    if (!foundUser) {
+        res.clearCookie('jwt', { httpOnly: true, sameSite: true, secure: true });
+        return res.sendStatus(204);
+    }
+
+    const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
+    const currentUser = {...foundUser, refreshToken: ''};
+    usersDB.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile (
+        path.join(__dirname, '..', 'model', 'users.json'),
+        JSON.stringify(usersDB.users)
+    );
+    res.clearCookie('jwt', { httpOnly: true, sameSite: true, secure: true });
+    res.sendStatus(204);
+}
+
+module.exports = { handleLogout }
