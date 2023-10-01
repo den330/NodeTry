73fe701594a5a210e2ab1f0e46695d3b8479f943
Yaxin Yuan <wo131485508@qq.com>
2023-10-01 14:06:35 -0400

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'https://www.yoursite.com', 
     'http://127.0.0.1:5500', 
     'http://localhost:3500', 
-    'https://www.google.ca'
+    'https://www.google.ca',
     'https://main.d2oi23u6roxp0m.amplifyapp.com'
 ];
 
